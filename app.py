d64e81838d74c6dfb45acf2dd455a033bc372a61
oxyum services <29GariglioSe@fortcherry.org>
2025-03-06 16:15:45 +0000

    push

--- app.py
+++ app.py
@@ -1,36 +1,37 @@
-from flask import Flask, send_from_directory, request
+from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, emit
+import cv2
+import numpy as np
 import base64
+from io import BytesIO
 from PIL import Image
-import io
-import tensorflow as tf
+import pyautogui  # Import pyautogui
 
-app = Flask(__name__, static_folder='public')
-
-# Load a pre-trained model (for example, MobileNetV2)
-model = tf.keras.applications.MobileNetV2(weights='imagenet')
+app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return send_from_directory(app.static_folder, 'index.html')
+    return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    data = request.json
-    image_data = data['image'].split(',')[1]
-    image = Image.open(io.BytesIO(base64.b64decode(image_data))).convert('RGB')  # Convert to RGB
+@socketio.on('screenshare')
+def handle_screenshare(data):
+    # Decode the image
+    img_data = base64.b64decode(data.split(',')[1])
+    img = Image.open(BytesIO(img_data))
+    img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
     
-    # Preprocess the image for the model
-    image = image.resize((224, 224))
-    image_array = tf.keras.preprocessing.image.img_to_array(image)
-    image_array = tf.keras.applications.mobilenet_v2.preprocess_input(image_array)
-    image_array = tf.expand_dims(image_array, 0)
+    # Save the image temporarily
+    cv2.imwrite('screenshot.png', img)
     
-    # Make a prediction
-    predictions = model.predict(image_array)
-    decoded_predictions = tf.keras.applications.mobilenet_v2.decode_predictions(predictions, top=1)[0]
-    observation = decoded_predictions[0][1]  # Get the class label
-
-    return {'observation': observation}, 200
+    # Process the image with pyautogui
+    detected_boxes = []
+    locations = pyautogui.locateAllOnScreen('target_image.png', confidence=0.8)
+    for loc in locations:
+        detected_boxes.append({"x": loc.left, "y": loc.top, "width": loc.width, "height": loc.height})
+    
+    # Emit the detected boxes to the client
+    emit('detection', detected_boxes)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
